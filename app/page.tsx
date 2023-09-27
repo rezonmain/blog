b6be34adc7b1373c17e41c9d038a537f77002d96
rezonmain <adiazgomez101@gmail.com>
2023-09-26 21:40:31 -0700

    feat(homePage): remove main tag

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { PostsList } from "@/components/PostList/PostList";
 
 export default async function Home() {
   return (
-    <main>
+    <>
       <h1>Welcome to my blog</h1>
       <h2>Posts:</h2>
       <PostsList />
-    </main>
+    </>
   );
 }
