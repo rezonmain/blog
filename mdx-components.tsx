26e22cf0164e2066c28a6e0fe01d1da1b412f8e5
rezonmain <adiazgomez101@gmail.com>
2023-10-01 13:35:49 -0700

    feat(mdx): added tag styles

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,7 @@
 import type { MDXComponents } from "mdx/types";
 import { FiExternalLink } from "react-icons/fi";
 import { PostHeader } from "@/components/PostHeader/PostHeader";
+import { Strings } from "./helpers/Strings.helpers";
 
 /**
  * Borrowed,
@@ -9,14 +10,26 @@ import { PostHeader } from "@/components/PostHeader/PostHeader";
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-    hr: (props) => <hr {...props} className="my-4 border-zinc-600" />,
+    hr: (props) => <hr {...props} className="my-8 border-zinc-600" />,
     a: (props) => (
       <a {...props} className="inline-flex items-center gap-0.5 underline">
         {props.children}
-        <FiExternalLink className="inline" />
+        {props.href?.startsWith("https://") && (
+          <FiExternalLink className="inline" />
+        )}
       </a>
     ),
-    p: (props) => <p {...props} className="mb-4 text-lg" />,
+    p: (props) => <p {...props} className="pb-8 text-lg" />,
+    h2: (props) => (
+      <h2
+        id={Strings.strToKebab(props.children?.toString() ?? "")}
+        {...props}
+        className="rounded-sm bg-zinc-400 pb-1.5 pl-1 text-3xl font-light text-zinc-950"
+      />
+    ),
+    h3: (props) => (
+      <h3 {...props} className="rounded-sm pb-1.5 pl-1 text-xl font-light" />
+    ),
     PostHeader: PostHeader,
   };
 }
