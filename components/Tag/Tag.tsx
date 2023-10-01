c2815e6f43ade797571c74b7ea88c4f51c342047
rezonmain <adiazgomez101@gmail.com>
2023-10-01 13:35:30 -0700

    feat(Tag): updated tag component style

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -2,7 +2,7 @@ import type { Props } from "./tag.types";
 
 const Tag: React.FC<Props> = ({ text }) => {
   return (
-    <span className="flex w-fit items-center justify-center rounded-sm bg-zinc-300 p-0.5 px-1.5 text-center text-xs font-bold tracking-wider text-zinc-950">
+    <span className="flex w-fit items-center justify-center rounded-sm bg-zinc-400 p-0.5 px-1.5 text-center text-xs font-bold tracking-wider text-zinc-950">
       {text}
     </span>
   );
