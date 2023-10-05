31477e23702fdfd28ac20b7fc8958489acb05785
rezonmain <adiazgomez101@gmail.com>
2023-10-05 13:39:06 -0700

    feat(globalLayout): add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Exo_2 } from "next/font/google";
 import { Header } from "@/components/Header/Header";
+import { Footer } from "@/components/Footer/Footer";
 
 const exo2 = Exo_2({ subsets: ["latin"] });
 
@@ -17,9 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={exo2.className}>
+      <body className={exo2.className + " pb-8"}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
