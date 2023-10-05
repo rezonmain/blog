64756193cf65b6e368c37356c25ce74bd467472d
rezonmain <adiazgomez101@gmail.com>
2023-10-05 15:36:56 -0700

    feat(PostList): styling

--- components/PostList/PostList.tsx
+++ components/PostList/PostList.tsx
@@ -6,11 +6,12 @@ const PostsList: React.FC<Props> = async ({ listType = "all" }) => {
   const posts = await listTypePostsGetters[listType]();
 
   return (
-    <ul>
-      {posts.map(({ title, href, name }) => (
-        <li key={name}>
-          <Link href={href}>
-            <h4>{title}</h4>
+    <ul className="list-disc rounded-md bg-zinc-900 p-8">
+      {posts.map(({ title, href, name, description }) => (
+        <li key={name} className="cursor-pointer p-4 hover:bg-zinc-800">
+          <Link href={href} className="inline">
+            <h3 className="text-xl font-semibold">{title}</h3>
+            <h4 className=" text-zinc-200">{description}</h4>
           </Link>
         </li>
       ))}
