db96201d6c6bd4c33c94448ff631a9491de89e86
rezonmain <adiazgomez101@gmail.com>
2023-10-05 15:37:26 -0700

    feat(wubbysChatters): remove wip

--- app/post/wubbys-chatters/page.tsx
+++ app/post/wubbys-chatters/page.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
 import WubbysChatters, { meta } from "./wubbys-chatters.mdx";
-import { Wip } from "@/components/Wip/Wip";
 
 export const metadata: Metadata = {
   ...meta,
 };
 
 export default function Page() {
-  return (
-    <>
-      <Wip />
-      <WubbysChatters />
-    </>
-  );
+  return <WubbysChatters />;
 }
