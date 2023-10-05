9a87fb7e4822f9349f4194b56b0bdba3a217b62c
rezonmain <adiazgomez101@gmail.com>
2023-10-05 15:36:14 -0700

    feat(homePage): styling layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { PostsList } from "@/components/PostList/PostList";
 
 export default async function Home() {
   return (
-    <>
-      <h1>Welcome to my blog</h1>
-      <h2>Posts:</h2>
+    <article className="mx-auto flex max-w-screen-md flex-col justify-center gap-8 p-8">
+      <h1 className="text-4xl font-bold">Hi, this is rezonmain&apos;s blog</h1>
+      <h2 className="text-2xl font-semibold">Latest posts</h2>
       <PostsList />
-    </>
+    </article>
   );
 }
