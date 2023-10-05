37364b8b472e29e3a868ebf5a9db23d579b9d385
rezonmain <adiazgomez101@gmail.com>
2023-10-05 15:17:18 -0700

    feat(routes): add external links

--- constants/routes.constants.ts
+++ constants/routes.constants.ts
@@ -2,4 +2,9 @@ const Routes = {
   home: "/",
 };
 
-export { Routes };
+const ExternalLinks = {
+  support:
+    "https://www.paypal.com/donate/?business=XAFQC6M9HK54E&no_recurring=0&item_name=your+donation+helps+feed+my+2+cats&currency_code=USD",
+};
+
+export { Routes, ExternalLinks };
