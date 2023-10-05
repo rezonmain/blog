59b6bdbc1fe77d6f1fbd71cf382eb18d9a127665
rezonmain <adiazgomez101@gmail.com>
2023-10-05 13:40:25 -0700

    feat(postLayout): add bottom border

--- app/post/layout.tsx
+++ app/post/layout.tsx
@@ -5,7 +5,7 @@ export default function PostLayout({
 }) {
   return (
     <main className="flex-row md:flex">
-      <article className="col-span-4 mx-auto max-w-screen-md border border-b-0 border-t-0 border-zinc-800 p-4">
+      <article className="col-span-4 mx-auto max-w-screen-md border border-t-0 border-zinc-800 p-4">
         {children}
       </article>
       <aside className="hidden w-1/4 md:block"></aside>
